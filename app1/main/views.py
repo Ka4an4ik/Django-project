886c2ef05b5fbff72338ca375a5349fa5c7c0ed8
Denis <den.karta.12@mail.ru>
2024-03-23 19:39:22 +0400

    Множество изменений (до автогенерации слагов)

--- app1/main/views.py
+++ app1/main/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from goods.models import Categories
+
+
 def index(request):
-    return render(request, 'main/index.html')
+
+
+    categories = Categories.objects.all
+
+    context = {
+
+        'categories' : categories
+    }
+
+    return render(request, 'main/index.html', context)
 
 
 def about(request):
